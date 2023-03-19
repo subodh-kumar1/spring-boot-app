3687b8ae56f76b1f7438cceece50adc1a36c41c1
Subodh Kumar <subodh.subodhkumar1@gmail.com>
2023-03-19 21:14:34 +0530

    Enable scheduling for task

--- src/main/java/com/accountingapp/subodh/SubodhApplication.java
+++ src/main/java/com/accountingapp/subodh/SubodhApplication.java
@@ -2,8 +2,10 @@ package com.accountingapp.subodh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SubodhApplication {
 
 	public static void main(String[] args) {
