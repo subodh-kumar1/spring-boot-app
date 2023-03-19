30a508f8ddde575b98a7ae825198876ce2465901
Subodh Kumar <subodh.subodhkumar1@gmail.com>
2023-03-20 00:27:21 +0530

    Handling Cors issue

--- src/main/java/com/accountingapp/subodh/configuration/WebConfig.java
+++ src/main/java/com/accountingapp/subodh/configuration/WebConfig.java
@@ -19,6 +19,11 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+            .maxAge(3600);
     }
 }
