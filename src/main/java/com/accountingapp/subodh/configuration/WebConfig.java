49ea152ee396b1bc0cab4954bf2bfe1f20811622
Subodh Kumar <subodh.subodhkumar1@gmail.com>
2023-03-20 01:01:28 +0530

    Reverse

--- src/main/java/com/accountingapp/subodh/configuration/WebConfig.java
+++ src/main/java/com/accountingapp/subodh/configuration/WebConfig.java
@@ -19,11 +19,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-            .allowedOrigins("*")
-            .allowedMethods("GET", "POST", "PUT", "DELETE")
-            .allowedHeaders("*")
-            .allowCredentials(true)
-            .maxAge(3600);
+        registry.addMapping("/**");
     }
 }
